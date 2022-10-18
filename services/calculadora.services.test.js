81f52e54995ef650bbef9d095f8906e7750896e6
EJPA5873 <EJPA5873@itau.cl>
2022-10-17 21:55:05 -0500

    Add test

--- services/calculadora.services.test.js
+++ services/calculadora.services.test.js
@@ -2,6 +2,28 @@ const CalculadoraService = require('../services/calculadora.services');
 
 const calculadoraService = new CalculadoraService();
 
+describe('Test- CalculadoraService', () => {
+
+  it('El valor esperado debe devolver la suma de dos números enteros', () => {
+    expect(calculadoraService.sumar(1,2)).toBe(3);
+  })
+
+  it('El valor esperado debe devolver la resta de dos números enteros', () => {
+    expect(calculadoraService.restar(8,2)).toBe(6);
+  })
+
+  it('El valor esperado debe devolver el producto de dos números enteros', () => {
+    expect(calculadoraService.multiplicar(5,2)).toBe(10);
+  })
+
+  it('El valor esperado debe devolver la devisión de dos números enteros', () => {
+    expect(calculadoraService.dividir(10,2)).toBe(5);
+  })
+
+})
+
+
+/*
 test('Test sumar()', () => {
   expect(calculadoraService.sumar(1,2)).toBe(3);
 });
@@ -17,3 +39,4 @@ test('Test multiplicar()', () => {
 test('Test dividir()', () => {
   expect(calculadoraService.dividir(10,2)).toBe(5);
 });
+*/
